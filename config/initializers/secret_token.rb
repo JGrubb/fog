4ff6a13f6f93bc5648d7798983828c59d38eed62
John Grubb <therealjohnnygrubb@gmail.com>
2012-08-07 13:54:28 -0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fog::Application.config.secret_token = 'd6bf289b5cf58c8b88ed2055c2cb127b400fde03a65fb2d7e584fdceb31be729c8c9d0b345a54ec21d62d48237f151308489d5c5fd68cd7a5f5054294d56c0ea'
