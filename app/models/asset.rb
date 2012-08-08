5031894873324b867084379d81d9e5eb935890e6
John Grubb <therealjohnnygrubb@gmail.com>
2012-08-08 12:42:15 -0400

    whoops

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -3,7 +3,7 @@ class Asset < ActiveRecord::Base
   attr_accessible :image
   belongs_to :imageable, :polymorphic => true
   has_attached_file :image, :styles => {
-    :large => "600x800"
+    :large => "600x800",
     :medium => "480x600",
     :small => "150x200",
     :thumb => "100x100"
