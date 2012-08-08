45477d077296dbb0ea54f266ad39eaac5524c9ad
John Grubb <therealjohnnygrubb@gmail.com>
2012-08-08 12:38:22 -0400

    adding index to assets

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120808162704) do
+ActiveRecord::Schema.define(:version => 20120808163651) do
 
   create_table "announcements", :force => true do |t|
     t.text     "body"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(:version => 20120808162704) do
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
   end
+
+  add_index "assets", ["imageable_id"], :name => "index_assets_on_imageable_id"
 
   create_table "pages", :force => true do |t|
     t.string   "title"
