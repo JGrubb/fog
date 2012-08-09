b3890277890ef3143770d7cd63d64828ac532c23
John Grubb <therealjohnnygrubb@gmail.com>
2012-08-09 12:21:32 -0400

    uploads, processing working

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,12 +1,12 @@
 class Teacher < ActiveRecord::Base
   
   extend FriendlyId
-  friendly_id :name
+  friendly_id :name, use: :slugged
   
-  has_one :picture, :as => :imageable
+  has_one :image, :as => :imageable, :class_name => "Asset", :dependent => :destroy
   
-  attr_accessible :bio, :name, :picture_attributes
+  attr_accessible :bio, :name, :image_attributes
   
-  accepts_nested_attributes_for :picture
+  accepts_nested_attributes_for :image
   
 end
