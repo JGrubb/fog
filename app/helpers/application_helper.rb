89298a8d008b45586a91bf317e00104331716ccd
John Grubb <therealjohnnygrubb@gmail.com>
2012-08-10 22:17:59 -0400

    page sublayout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,11 @@ module ApplicationHelper
     
   end
   
+  def parent_layout(layout)
+    @view_flow.set(:layout,output_buffer)
+    self.output_buffer = render(:file => "layouts/#{layout}")
+  end
+  
   def dat_markdown(text)
     options = {
       :autolink => true,
