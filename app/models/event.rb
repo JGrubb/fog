1d96f25afc3a642e9e7607d67d93d18c3773a6c0
John Grubb <therealjohnnygrubb@gmail.com>
2012-08-25 10:58:58 -0600

    removing description validation from event

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,5 +3,5 @@ class Event < ActiveRecord::Base
   has_many :occurrences, dependent: :destroy
   accepts_nested_attributes_for :occurrences, allow_destroy: true
   attr_accessible :description, :title, :occurrences_attributes
-  validates :title, :description, presence: true
+  validates :title, :presence: true
 end
