f1c8a9ffed64ba95faaa444c0ba4cc6a5fe9d088
John Grubb <therealjohnnygrubb@gmail.com>
2013-03-25 07:11:27 -0400

    typo

--- app/models/announcement_attachment.rb
+++ app/models/announcement_attachment.rb
@@ -4,7 +4,7 @@ class AnnouncementAttachment < ActiveRecord::Base
   has_attached_file :file,
                     :storage => :s3,
                     :s3_credentials => {
-                      :bucket             => S3_BUCKET_NAME,
+                      :bucket             => S3_BUCKET,
                       :access_key_id      => S3_ACCESS_KEY,
                       :secret_access_key  => S3_SECRET_KEY
                     }
