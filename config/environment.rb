cf8d7564dab263e846dd93cdaf116b9f84f84cd3
John Grubb <therealjohnnygrubb@gmail.com>
2013-03-26 00:54:23 +0000

    config

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 require File.expand_path('../s3', __FILE__)
+require File.expand_path('../email', __FILE__)
 
 # Initialize the rails application
 Fog::Application.initialize!
